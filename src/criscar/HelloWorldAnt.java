1b5ceafb0840b9986ac5cbbbe84d47e691c5e20e
chrek <chrek@ymail.com>
2021-03-23 23:17:24 +0100

    first commit

---
+++ src/criscar/HelloWorldAnt.java
@@ -0,0 +1,10 @@
+package criscar;
+
+public class HelloWorldAnt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World from Ant");
+	}
+
+}

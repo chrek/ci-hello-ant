cb88603aa0a383ec2e0928ef3821eca9e80ee922
chrek <chrek@ymail.com>
2021-03-26 19:16:51 +0100

    add Log4j

---
+++ README.md
@@ -0,0 +1,10 @@
+CI_HELLO_ANT
+------------
+
+Using Apache Ant to build Java application
+
+Built-in tasks are used to compile, assemble, test and run the application with the aid of a build file.
+
+Log4j in Java is also provided using the property file to implement Logging.
+
+This application is under Continuous Integration
--- src/criscar/HelloWorldAnt.java
+++ src/criscar/HelloWorldAnt.java
@@ -1,10 +1,15 @@
 package criscar;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
 
 public class HelloWorldAnt {
+	public static Logger log = Logger.getLogger(HelloWorldAnt.class);
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World from Ant");
+		BasicConfigurator.configure();
+		log.debug("Hi there from the logger");
+		//System.out.println("Hello World from Ant and Log4j");
+		log.info("Hello World from Ant");
 	}
 
 }
